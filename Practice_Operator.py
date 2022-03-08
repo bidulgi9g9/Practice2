6d33899d06fe4ebd9eafd8e803d69cbfcae1cd5b
LeeMinHyuk <hl2duu2@naver.com>
2022-03-08 16:04:26 +0900

    operatir commit

---
+++ Practice_Operator.py
@@ -0,0 +1,29 @@
+print(1+1)
+print(3-2)
+print(5*2)
+print(6/3)
+
+print(2**3) #제곱값
+print(5%3) #나머지값
+print(10%3)
+print(5//3) #몫의값
+
+print(10 > 3) #True
+print(4 >= 7) #False
+print(5 <= 5) #True
+
+print(3 == 3) #앞과 뒤의 값이 같다
+print(3 == 5)
+print(3 + 4 == 7)
+
+print(1 != 3) #앞과 뒤가 같지 않다 True
+print(not(1 != 3)) #False
+
+print((3 > 0) and (3 < 5)) #True
+print((3 > 0) & (3 < 5)) #True
+
+print((3 > 0) or (3 > 5)) #or조건일때 둘중 하나만 맞으면 True 출력  or=|
+print((3 > 0) | (3 > 5)) #True
+
+print(5 > 4 > 3) #True
+print(5 > 4 > 7) #False
